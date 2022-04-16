150a82b07121a5998b6805ee0aafce8f920c1db0
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-16 23:20:41 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Component/Shear/Header/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- src/Component/Shear/Header/Header.js
+++ src/Component/Shear/Header/Header.js
@@ -1,7 +1,37 @@
 import React from "react";
-
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../../../Assets/Images/logo.png";
+import "./Header.css";
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar sticky="top" className="shadow-sm" bg="white" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img style={{ width: "200px" }} src={logo} alt="" />{" "}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="m-auto">
+              <Nav.Link className="link" href="#home">
+                Home
+              </Nav.Link>
+              <Nav.Link className="link" href="#link">
+                Blogs
+              </Nav.Link>
+              <Nav.Link className="link" href="#link">
+                About
+              </Nav.Link>
+              <Nav.Link className="link" href="#link">
+                Services
+              </Nav.Link>
+            </Nav>
+            <button className="loginBtn">Login</button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
