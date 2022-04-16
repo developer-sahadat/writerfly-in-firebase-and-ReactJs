6c58c2c58976fcb1babe935af70d7bec503c88ba
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-16 23:50:00 +0600

    banner section added

--- src/Component/Home/Banner/Banner.js
+++ src/Component/Home/Banner/Banner.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { Carousel } from "react-bootstrap";
+import banner1 from "../../../Assets/Images/banner1.png";
+import banner2 from "../../../Assets/Images/banner2.png";
+import banner3 from "../../../Assets/Images/banner3.png";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={banner3} alt="First slide" />
+          <Carousel.Caption>
+            <h3 className="text-black">Creative writer</h3>
+            <p className="text-black">
+              Attributes Are you looking for your content?
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={banner2} alt="Second slide" />
+
+          <Carousel.Caption>
+            <h3 className="text-black">Creative writer</h3>
+            <p className="text-black">
+              Attributes Are you looking for your content?
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={banner1} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3 className="text-black">Creative writer</h3>
+            <p className="text-black">
+              Attributes Are you looking for your content?
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
