625f0613f2e074320b19ec0232b56fe6149b13cf
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-17 22:17:45 +0600

    about us section added

--- src/Component/Pages/About/About.js
+++ src/Component/Pages/About/About.js
@@ -1,7 +1,38 @@
 import React from "react";
+import "./About.css";
+import personal from "../../../Assets/Images/personal.png";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container p-2">
+        <h1 className="title">
+          About <span>Me</span>
+        </h1>
+        <div className="row row-cols-md-2 justify-content-center align-items-center">
+          <div className="personal">
+            <div>
+              <img src={personal} className="img-fluid" alt="personal" />
+            </div>
+          </div>
+          <div>
+            <div className="personal-info">
+              <h3> Md Shahadat Hossain</h3>
+              <h6>Junior Web Developer</h6>
+              <p>
+                Inshallah, I tried my best to do the course. I have a plan. I
+                will form a team after completing the course. Already taken a
+                few for my team. I will work with the team at the end of the
+                course InshaAllah. I will work with LinkedIn and email marketing
+                and bring in clients from outside and work with the team. So
+                that our team can do a lot more work.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Component/Pages/Services/Services.js
+++ src/Component/Pages/Services/Services.js
@@ -8,7 +8,7 @@ const Services = () => {
   return (
     <div>
       <div className="container">
-        <h1 className="services-title">
+        <h1 className="title">
           Our <span>Content Write</span> Services<span>.</span>
         </h1>
         <div className="row row-cols-lg-3 row-cols-md-2 g-3 mb-4 mt-3">
